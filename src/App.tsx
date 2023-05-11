3145a62ae34221a46a4cb13d69a19a197f5a2ff0
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-05-11 23:53:37 +0100

    Styling summary section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import ReactionIcon from './assets/images/icon-reaction.svg'
+import MemoryIcon from './assets/images/icon-memory.svg'
+import VerbalIcon from './assets/images/icon-verbal.svg'
+import VisualIcon from './assets/images/icon-visual.svg'
+
 function App() {
   return (
     <div className='h-screen w-screen flex items-center justify-center bg-white font-hanken-grotesk text-lg box-border'>
@@ -9,54 +14,87 @@ function App() {
           <h2 className='text-light-lavander text-2xl'>Your Result</h2>
           <div
             id='circle'
-            className='w-48 h-48 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full flex flex-col items-center justify-center'
+            className='w-48 h-48 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full flex flex-col items-center justify-center my-6'
           >
             <h1 className='text-6xl text-white font-extrabold'>76</h1>
-            <p className='text-light-lavander text-sm mt-2'>of 100</p>
+            <p className='text-light-lavander text-lg mt-2'>of 100</p>
           </div>
-          <h2 className='text-3xl text-white font-bold'>Great</h2>
+          <h2 className='text-3xl text-white font-bold mb-4'>Great</h2>
           <p className='text-lg text-light-lavander font-medium'>
             You scored higher than 65% of the people who haven taken these
             tests.
           </p>
         </div>
 
-        <div id='summary' className='py-12 px-10'>
-          <h2>Summary</h2>
-          <div id='reaction'>
-            <span>Icon</span>
-            Reaction
-            <div id='score'>
-              <span>80</span>
-              <p>/ 100</p>
+        <div
+          id='summary'
+          className='w-1/2 py-12 px-10 flex flex-col justify-between'
+        >
+          <h2 className='text-2xl text-dark-gray-blue'>Summary</h2>
+
+          <div>
+            <div
+              id='reaction'
+              className='mb-4 flex flex-row justify-between px-4 py-2 rounded-lg bg-light-red-bg'
+            >
+              <p className='flex text-light-red'>
+                <img src={ReactionIcon} className='mr-4' />
+                Reaction
+              </p>
+              <div id='score'>
+                <p className='text-dark-gray-blue-faded'>
+                  <span className='text-dark-gray-blue mr-1'>80</span> / 100
+                </p>
+              </div>
             </div>
-          </div>
-          <div id='memory'>
-            <span>Icon</span>
-            Memory
-            <div id='score'>
-              <span>92</span>
-              <p>/ 100</p>
+            <div
+              id='memory'
+              className='mb-4 flex flex-row justify-between px-4 py-2 rounded-lg bg-orange-yellow-bg'
+            >
+              <p className='flex text-orange-yellow'>
+                <img src={MemoryIcon} className='mr-4' />
+                Memory
+              </p>
+              <div id='score'>
+                <p className='text-dark-gray-blue-faded'>
+                  <span className='text-dark-gray-blue mr-1'>92</span> / 100
+                </p>
+              </div>
             </div>
-          </div>
-          <div id='verbal'>
-            <span>Icon</span>
-            Verbal
-            <div id='score'>
-              <span>61</span>
-              <p>/ 100</p>
+            <div
+              id='verbal'
+              className='mb-4 flex flex-row justify-between px-4 py-2 rounded-lg bg-green-teal-bg'
+            >
+              <p className='flex text-green-teal'>
+                <img src={VerbalIcon} className='mr-4' />
+                Verbal
+              </p>
+              <div id='score'>
+                <p className='text-dark-gray-blue-faded'>
+                  <span className='text-dark-gray-blue mr-1'>61</span> / 100
+                </p>
+              </div>
             </div>
-          </div>
-          <div id='visual'>
-            <span>Icon</span>
-            Visual
-            <div id='score'>
-              <span>72</span>
-              <p>/ 100</p>
+            <div
+              id='visual'
+              className='flex flex-row justify-between px-4 py-2 rounded-lg bg-cobalt-blue-bg'
+            >
+              <p className='flex text-cobalt-blue'>
+                <img src={VisualIcon} className='mr-4' />
+                Visual
+              </p>
+              <div id='score'>
+                <p className='text-dark-gray-blue-faded'>
+                  <span className='text-dark-gray-blue mr-1'>72</span> / 100
+                </p>
+              </div>
             </div>
           </div>
 
-          <button type='button' className='bg-purple-500'>
+          <button
+            type='button'
+            className='text-white text-lg font-medium bg-dark-gray-blue hover:bg-gradient-to-b hover:from-light-slate-blue hover:to-light-royal-blue rounded-full py-2 px-6'
+          >
             Continue
           </button>
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,13 @@ module.exports = {
     extend: {
       colors: {
         'light-red': 'hsl(0, 100%, 67%)',
+        'light-red-bg': 'hsla(0, 100%, 67%, 0.1)',
         'orange-yellow': 'hsl(39, 100%, 56%)',
+        'orange-yellow-bg': 'hsla(39, 100%, 56%, 0.1)',
         'green-teal': 'hsl(166, 100%, 37%)',
+        'green-teal-bg': 'hsla(166, 100%, 37%, 0.1)',
         'cobalt-blue': 'hsl(234, 85%, 45%)',
+        'cobalt-blue-bg': 'hsla(234, 85%, 45%, 0.1)',
         'light-slate-blue': 'hsl(252, 100%, 67%)',
         'light-royal-blue': 'hsl(241, 81%, 54%)',
         'violet-blue': 'hsla(256, 72%, 46%, 1)',
@@ -16,6 +20,7 @@ module.exports = {
         'pale-blue': 'hsl(221, 100%, 96%)',
         'light-lavander': 'hsl(241, 100%, 89%)',
         'dark-gray-blue': 'hsl(223, 30%, 27%)',
+        'dark-gray-blue-faded': 'hsla(209, 23%, 22%, 0.4)',
       },
       fontFamily: {
         'hanken-grotesk': ['Hanken Grotesk', 'sans-serif'],
