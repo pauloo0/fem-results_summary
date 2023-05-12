751d1385045b4487a1b2771ae4ba0e835b029c17
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-05-12 21:51:58 +0100

    Adding mobile compatibility and a README file.

---
+++ README.md
@@ -0,0 +1,57 @@
+# Frontend Mentor - Results summary component solution
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./src/assets/images/solution-screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://www.frontendmentor.io/solutions/responsive-resultsummary-component-with-reacttypescripttailwindcss-s-AQetkvzT)
+- Live Site URL: [Add live site URL here](https://fem-results-summary-mu.vercel.app)
+
+## My process
+
+### Built with
+
+- [React](https://reactjs.org/) - JS library
+- [Typescript](https://www.typescriptlang.org/) - JS Superset
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
+
+### What I learned
+
+This being my first real project working with more complex css, I learned a lot on how I could customize my own values on tailwindCSS and also learned the basics of using gradients on tailwindCSS.
+I also learned more about creating a responsive design.
+
+### Continued development
+
+Overall I still need to learn a lot about responsive design. Maybe consider a Mobile-first workflow. From this experience I found it tricky to get the mobile design accurate.
+I want to continue learning about placement and positioning because I found that my widths, heights, margins, paddings and such didn't came out the same as the design.
+
+## Author
+
+- Website - [Add your name here](https://www.paulojoliveira.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@paul0_oLiveir4](https://twitter.com/paul0_oLiveir4)
--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,30 @@ import VisualIcon from './assets/images/icon-visual.svg'
 
 function App() {
   return (
-    <div className='h-screen w-screen flex items-center justify-center bg-white font-hanken-grotesk text-lg box-border'>
+    <div className='h-screen w-screen flex sm:items-center sm:justify-center bg-white font-hanken-grotesk text-lg box-border'>
       <div className='max-w-4xl flex flex-col sm:flex-row rounded-3xl shadow-2xl bg-white'>
         <div
           id='score'
-          className='w-1/2 rounded-3xl bg-gradient-to-b from-light-slate-blue to-light-royal-blue py-12 px-10 flex flex-col items-center justify-between text-center'
+          className='sm:w-1/2 rounded-b-3xl sm:rounded-3xl bg-gradient-to-b from-light-slate-blue to-light-royal-blue py-8 sm:py-12 px-10 flex flex-col items-center justify-between text-center'
         >
-          <h2 className='text-light-lavander text-2xl'>Your Result</h2>
+          <h2 className='text-light-lavander text-xl sm:text-2xl'>
+            Your Result
+          </h2>
           <div
             id='circle'
-            className='w-48 h-48 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full flex flex-col items-center justify-center my-6'
+            className='w-36 h-36 sm:w-48 sm:h-48 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full flex flex-col items-center justify-center my-6'
           >
-            <h1 className='text-6xl text-white font-extrabold'>76</h1>
-            <p className='text-light-lavander text-lg mt-2'>of 100</p>
+            <h1 className='text-5xl sm:text-6xl text-white font-extrabold'>
+              76
+            </h1>
+            <p className='text-light-lavander text-base sm:text-lg mt-2'>
+              of 100
+            </p>
           </div>
-          <h2 className='text-3xl text-white font-bold mb-4'>Great</h2>
-          <p className='text-lg text-light-lavander font-medium'>
+          <h2 className='text-2xl sm:text-3xl text-white font-bold mb-4'>
+            Great
+          </h2>
+          <p className='text-base sm:text-lg text-light-lavander font-medium'>
             You scored higher than 65% of the people who haven taken these
             tests.
           </p>
@@ -28,11 +36,11 @@ function App() {
 
         <div
           id='summary'
-          className='w-1/2 py-12 px-10 flex flex-col justify-between'
+          className='sm:w-1/2 py-8 sm:py-12 px-10 flex flex-col justify-between'
         >
           <h2 className='text-2xl text-dark-gray-blue'>Summary</h2>
 
-          <div>
+          <div className='my-6'>
             <div
               id='reaction'
               className='mb-4 flex flex-row justify-between px-4 py-2 rounded-lg bg-light-red-bg'
